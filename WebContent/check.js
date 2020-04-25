a3216bd6e178347aab65401070e0cddff819e1ec
Nicholas Martinez <nicok.martinez@gmail.com>
2020-04-25 16:53:34 -0400

    Fields on the create account screen and homepage are now remembered.

--- WebContent/check.js
+++ WebContent/check.js
@@ -91,4 +91,19 @@ function signOut() {
 	loggedIn = false;
 }
 
+//Save the value function - save it to localStorage as (ID, VALUE)
+function saveValue(e){
+    var id = e.id;  // get the sender's id to save it . 
+    var val = e.value; // get the value. 
+    localStorage.setItem(id, val);// Every time user writing something, the localStorage's value will override . 
+}
+
+//get the saved value function - return the value of "v" from localStorage. 
+function getSavedValue  (v){
+    if (!localStorage.getItem(v)) {
+        return "";// You can change this to your defualt value. 
+    }
+    return localStorage.getItem(v);
+}
+
 
